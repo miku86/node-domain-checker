5ff192d2146c4b50807fe72d4c4b40285fb23112
miku86 <miku86coding@gmail.com>
2019-08-31 16:42:26 +0200

    add color to output messages

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 const readline = require('readline');
 const { https } = require('follow-redirects');
+const chalk = require('chalk');
 
 console.log('Welcome to the Domain Checker');
 const FILE_NAME = process.argv[2];
 
+const successCodes = [200];
+const success = chalk.bgGreen.black;
+
+const warningCodes = [301, 302];
+const warning = chalk.bgYellow.black;
+
+const errorCodes = [403, 404];
+const error = chalk.bgRed.black;
+
 const isUrlReachable = (url) => {
-  const req = https.get(url, (res) => {
-    console.log(`${url}: ${res.statusCode}`);
+  const request = https.get(url, (response) => {
+    const { statusCode } = response;
+
+    if (successCodes.includes(statusCode)) {
+      console.log(`${success(`${url}: ${statusCode}`)}`);
+    } else if (warningCodes.includes(statusCode)) {
+      console.log(`${warning(`${url}: ${statusCode}`)}`);
+    } else if (errorCodes.includes(statusCode)) {
+      console.log(`${error(`${url}: ${statusCode}`)}`);
+    } else {
+      console.log(`${url}: ${statusCode}`);
+    }
   });
-  req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
+  request.on('error', (error) => {
+    console.error(`problem with request: ${error.message}`);
   });
-  req.end();
+  request.end();
 };
 
 const processLineByLine = async () => {
