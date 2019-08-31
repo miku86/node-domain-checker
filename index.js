b91a4b554aac2dffead106958e07f15b7e37a237
miku86 <miku86coding@gmail.com>
2019-08-31 16:22:43 +0200

    add function to check if url is reachable

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
 const readline = require('readline');
+const http = require('http');
+const url = require('url');
 
 console.log('Welcome to the Domain Checker');
 const FILE_NAME = process.argv[2];
+
+// check if domain is reachable
+const isUrlReachable = (url) => {
+  const { hostname } = new URL(url);
+
+  const options = {
+    hostname
+  };
+
+  const req = http.get(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+  });
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+  req.end();
+};
 
 // read file line by line
 const processLineByLine = async () => {
@@ -14,7 +33,7 @@ const processLineByLine = async () => {
   });
 
   for await (const line of lines) {
-    console.log(`${line}`);
+    isUrlReachable(line);
   }
 };
 
