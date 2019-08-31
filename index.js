ed2e475dc398045fbf0992996688c1dd2db13516
miku86 <miku86coding@gmail.com>
2019-08-31 16:57:23 +0200

    add regex check for url

--- index.js
+++ index.js
@@ -15,6 +15,36 @@ const warning = chalk.bgYellow.black;
 const errorCodes = [403, 404];
 const error = chalk.bgRed.black;
 
+const isValidUrl = (url) => {
+  const regexToCheckIfUrlIsValid = new RegExp(
+    '^' +
+      '(?:(?:(?:https?|ftp):)?\\/\\/)' +
+      '(?:\\S+(?::\\S*)?@)?' +
+      '(?:' +
+      '(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
+      '(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
+      '(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
+      '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
+      '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
+      '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
+      '|' +
+      '(?:' +
+      '(?:' +
+      '[a-z0-9\\u00a1-\\uffff]' +
+      '[a-z0-9\\u00a1-\\uffff_-]{0,62}' +
+      ')?' +
+      '[a-z0-9\\u00a1-\\uffff]\\.' +
+      ')+' +
+      '(?:[a-z\\u00a1-\\uffff]{2,}\\.?)' +
+      ')' +
+      '(?::\\d{2,5})?' +
+      '(?:[/?#]\\S*)?' +
+      '$',
+    'i'
+  );
+  return url.match(regexToCheckIfUrlIsValid);
+};
+
 const isUrlReachable = (url) => {
   const request = https.get(url, (response) => {
     const { statusCode } = response;
@@ -44,7 +74,11 @@ const processLineByLine = async () => {
   });
 
   for await (const line of lines) {
-    isUrlReachable(line);
+    if (isValidUrl(line)) {
+      isUrlReachable(line);
+    } else {
+      console.log(error(`"${line}" is not valid, please enter a correct URL!`));
+    }
   }
 };
 
