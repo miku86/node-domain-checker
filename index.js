ee43fe31ed982498326fb22a868e98224dbab79f
miku86 <miku86coding@gmail.com>
2019-08-31 16:28:29 +0200

    remove unused package and comments

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const readline = require('readline');
 const { https } = require('follow-redirects');
-const url = require('url');
 
 console.log('Welcome to the Domain Checker');
 const FILE_NAME = process.argv[2];
 
-// check if domain is reachable
 const isUrlReachable = (url) => {
   const req = https.get(url, (res) => {
     console.log(`${url}: ${res.statusCode}`);
@@ -17,7 +15,6 @@ const isUrlReachable = (url) => {
   req.end();
 };
 
-// read file line by line
 const processLineByLine = async () => {
   const fileStream = fs.createReadStream(FILE_NAME);
 
