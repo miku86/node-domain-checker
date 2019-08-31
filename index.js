c9eb445be261fcd6726ed7f2e6f20ecf5f9c7898
miku86 <miku86coding@gmail.com>
2019-08-31 16:03:05 +0200

    add function to read text file line by line

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const fs = require('fs');
+const readline = require('readline');
+
 console.log('Welcome to the Domain Checker');
 const FILE_NAME = process.argv[2];
 
-console.log(FILE_NAME);
+// read file line by line
+const processLineByLine = async () => {
+  const fileStream = fs.createReadStream(FILE_NAME);
+
+  const lines = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  for await (const line of lines) {
+    console.log(`${line}`);
+  }
+};
+
+processLineByLine();
